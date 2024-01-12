b070716ac847a12a5ecf7845d791d19ca1a52764
Hiiragi0831 <glazunovden@yandex.ru>
2024-01-12 16:12:29 +0300

    init

---
+++ src/components/header.svelte
@@ -0,0 +1,16 @@
+<header>
+	<nav>
+		<a href="/">link</a>
+		<a href="/">link</a>
+		<a href="/">link</a>
+		<a href="/">link</a>
+		<a href="/">link</a>
+	</nav>
+</header>
+
+<style style lang="scss">
+    nav {
+		display: flex;
+		gap: 20px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Header from '../components/header.svelte';
+</script>
+
+<Header />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style style lang="scss">
+    h1 {
+       font-size: 3rem;
+    }
+</style>
